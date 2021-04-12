178f85c230285ae59ff11d80f51b3aa851a8d356
Bombohog <70882697+Bombohog@users.noreply.github.com>
2021-04-12 14:08:57 +0200

    Initial commit

---
+++ app/src/main/java/dk/awayoflife/androiddevfun_2/OrderActivity.java
@@ -0,0 +1,13 @@
+package dk.awayoflife.androiddevfun_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
